4c5e1bc0a1a8f01c1a7abee977c383875984e19e
Khens Code <davidsamwelson@gmail.com>
2025-07-15 19:56:33 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("container");
+const lockBtn = document.getElementById("lock");
+
+lockBtn.addEventListener('click' , () => {
+    container.classList.add('.height');
+})
